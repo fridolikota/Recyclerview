7bfd4ec1bdf37cda7310549ed43fb2a5e4cd3238
Ryz <fridolonkota@gmail.com>
2023-05-28 22:31:30 +0700

    Recyclerview APP

--- app/src/main/java/com/fridolinkota/recyclerview/MainActivity.kt
+++ app/src/main/java/com/fridolinkota/recyclerview/MainActivity.kt
@@ -28,8 +28,33 @@ class MainActivity : AppCompatActivity() {
 
         binding.list.adapter = AdapterTeamBola(this,ListPemain,object :AdapterTeamBola.OnClickListener{
             override fun DetailData(item: Pemain?) {
-                TODO("Not yet implemented")
+                Dialog(this@MainActivity).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.detail_data_pemain)
+
+                    val image = this.findViewById<ImageView>(R.id.image_pemain)
+                    val nama = this.findViewById<TextView>(R.id.txtNamaPemain)
+
+                    val posisi = this.findViewById<TextView>(R.id.txtPosisi)
+                    val tinggi = this.findViewById<TextView>(R.id.txtTinggi)
+                    val tempatlahir = this.findViewById<TextView>(R.id.txtTempatlahir)
+                    val tgllahir = this.findViewById<TextView>(R.id.txtTanggallahir)
+                    val btn = this.findViewById<Button>(R.id.btnClose)
+
+                    image.setImageResource(item?.foto ?:0)
+                    nama.text ="${item?.nama}"
+                    posisi.text ="${item?.posisi}"
+                    tinggi.text ="${item?.tinggi}"
+                    tempatlahir.text ="${item?.tempatlahir}"
+                    tgllahir.text ="${item?.tgllahir}"
+                    btn.setOnClickListener {
+                        this.dismiss()
+                    }
+
+                }.show()
             }
 
         })
     }
+}
